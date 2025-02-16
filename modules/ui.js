df8d37d94737b67ada533bba124bfca628114f11
ilkckn <musa.cekcen@dci-student.org>
2025-02-16 23:42:15 +0100

    load more button updated

--- modules/ui.js
+++ modules/ui.js
@@ -94,7 +94,7 @@ const createLoadMoreButton = () => {
   loadMoreButton.id = "loadMoreButton";
   loadMoreButton.className =
     "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-4";
-  loadMoreButton.textContent = "Daha Fazla Yükle";
+  loadMoreButton.textContent = "Load More";
 
   loadMoreButton.addEventListener("click", () => {
     visibleItems += 10; 
