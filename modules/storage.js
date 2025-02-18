14134d9aeb71dd405ba48296fbed4e90d7371e5d
ilkckn <musa.cekcen@dci-student.org>
2025-02-18 23:00:35 +0100

    fixing and add favorite btn

---
+++ modules/storage.js
@@ -0,0 +1,15 @@
+export const addFavorite = (id, name, type, height, weight) => {
+  const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+
+  if (!favorites.some((pokemon) => pokemon.id === id)) {
+    favorites.push({ id, name, type, height, weight });
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+  }
+};
+
+export const removeFavorite = (id) => {
+  let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+
+  favorites = favorites.filter((pokemon) => pokemon.id !== id);
+  localStorage.setItem("favorites", JSON.stringify(favorites));
+};
--- modules/ui.js
+++ modules/ui.js
@@ -1,4 +1,9 @@
-import { pokeAPI } from "./network.js";
+import { pokeAPI } from "../modules/network.js";
+import { addFavorite, removeFavorite } from "./storage.js";
+
+const listContainer = document.querySelector("#list-container");
+const body = document.querySelector("body");
+
 export const pokeData = async () => {
   try {
     const data = await pokeAPI();
@@ -14,17 +19,50 @@ export const pokeData = async () => {
         }
         const pokemon = await result.json();
         const cardigan = document.createElement("div");
-        cardigan.className = "w-[100px] bg-red-500 p-2 border rounded-md";
+        cardigan.className =
+          "w-[12rem] h-[15rem] flex flex-col justify-between items-center bg-white border-[1px] border-slate-300 rounded-md shadow-md pb-[.7rem] relative cursor-pointer";
         cardigan.innerHTML = `
-          <p><strong>${pokemon.name.toUpperCase()}</strong></p>
-          <p>Height: ${pokemon.height}</p>
-          <p>Weight: ${pokemon.weight}</p>
-          <p>Type: ${pokemon.types.map(typeInfo => typeInfo.type.name).join(", ")}</p>
-          <div>
-            <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}" class="w-20 h-20">
-          </div>
+        <div class="w-full h-[8rem] flex justify-center items-center">
+          <img src="${pokemon.sprites.front_default}" alt="${
+          pokemon.name
+        }" class="w-full h-[100%] bg-gray-200">
+        </div>
+          <p class="text-[1rem] font-medium">${pokemon.name.toUpperCase()}</p>
+          <p class="text-[.7rem]">Height: ${pokemon.height}</p>
+          <p class="text-[.7rem]">Weight: ${pokemon.weight}</p>
+          <p class="text-[.7rem]">Type: ${pokemon.types
+            .map((typeInfo) => typeInfo.type.name)
+            .join(", ")}</p>
         `;
-        document.getElementById("cards").appendChild(cardigan);
+        listContainer.appendChild(cardigan);
+        cardigan.addEventListener("click", () => {
+          const modal = document.createElement("div");
+          modal.className =
+            "fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center";
+          const modalContent = document.createElement("div");
+          modalContent.className =
+            "w-[30rem] h-[40rem] bg-white border-[1px] border-slate-300 rounded-md shadow-md flex flex-col justify-between items-center p-4";
+          modal.appendChild(modalContent);
+          body.appendChild(modal);
+        });
+
+        const favoriteBtn = document.createElement("img");
+        favoriteBtn.src = "../src/assets/images/pokeballs/pokeball-1.png";
+        favoriteBtn.alt = "Favorite";
+        favoriteBtn.className =
+          "w-[1.5rem] h-[1.5rem] absolute top-2 right-2 cursor-pointer";
+        cardigan.appendChild(favoriteBtn);
+        favoriteBtn.addEventListener("click", (event) => {
+          event.stopPropagation();
+          
+          if (favoriteBtn.src.includes("pokeball-1.png")) {
+            favoriteBtn.src = "../src/assets/images/pokeballs/pokeball-2.png";
+            addFavorite(id,name,type,height,weight);
+          } else {
+            favoriteBtn.src = "../src/assets/images/pokeballs/pokeball-1.png";
+            removeFavorite(id);
+          }
+        });
       } catch (error) {
         console.error("Error fetching Pokémon data:", error);
       }
